8a650849edf96f6ed16b75e3ae004aebdea01662
relativeabsolute <jburke4126@gmail.com>
2014-03-05 21:51:10 -0500

    Added more to address generation code.

--- src/bitlawui/protocol/address.py
+++ src/bitlawui/protocol/address.py
@@ -3,7 +3,25 @@
 
 from ..pyelliptic.ecc import *
 from ..utilities.threadutils import *
+from ..utilities.constants import *
 import hashlib
+from struct import *
+
+class Address:
+    def __init__(self, hashValue, version=VERSION):
+        self.version = version
+        self.hashValue = hashValue
+
+    def encodeVersion(self):
+        return pack('>B', self.version)
+
+    def encode(self):
+        a = self.encodeVersion() + self.hashValue
+        safePrint(a)
+        sha = hashlib.new('sha512')
+        sha.update(a)
+        sha.update(sha.digest())
+        checksum = sha.digest()[0:4]
 
 def genAddress():
     curve = ECC()
@@ -12,5 +30,9 @@ def genAddress():
     sha.update(pub_key)
     ripemd = hashlib.new('ripemd160')
     ripemd.update(sha.digest())
-    safePrint(ripemd.digest())
-    return None
+    sha.update(ripemd.digest())
+    ripemd.update(sha.digest())
+    #safePrint(ripemd.digest())
+    a = Address(ripemd.digest())
+    a.encode()
+    return a
---
+++ src/bitlawui/utilities/constants.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2014 Johan Burke
+# Distributed under the MIT software license.  See http://www.opensource.org/licenses/mit-license.php.
+
+# This file includes constants used by several files.  DO NOT CHANGE THEIR VALUES.
+VERSION = 1
+ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ+-"
